5628c969df83c5b4ed84e9f3fc58c973a1f90173
Nico Müller <nico.mueller@mni.thm.de>
2017-06-05 20:16:36 +0200

    wip

--- public/js/dropzoneconfig.js
+++ public/js/dropzoneconfig.js
@@ -1,27 +1,13 @@
-Dropzone.options.uploadWidget = {
+Dropzone.options.uploadArea = {
   paramName: 'file',
   maxFilesize: 2, // MB
-  maxFiles: 1,
-  dictDefaultMessage: 'Drag an image here to upload, or click to select one',
-  acceptedFiles: 'image/*',
+  maxFiles: 10,
+  dictDefaultMessage: 'Drag an your sound here (manual reload required)',
+  acceptedFiles: ".mp3",
   init: function() {
     this.on('success', function( file, resp ){
       console.log( file );
       console.log( resp );
     });
-    this.on('thumbnail', function(file) {
-      if ( file.width < 640 || file.height < 480 ) {
-        file.rejectDimensions();
-      }
-      else {
-        file.acceptDimensions();
-      }
-    });
-  },
-  accept: function(file, done) {
-    file.acceptDimensions = done;
-    file.rejectDimensions = function() {
-      done('The image must be at least 640 x 480px')
-    };
   }
 };
